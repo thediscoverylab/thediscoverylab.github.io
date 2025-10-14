c66686e3eca70cbcea0f921493cf6cf45708c289
Alissa Rivero <alissa.rivero@duke.edu>
2025-10-14 13:37:13 -0400

    Add name and description to config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Alissa Rivero | Developmental Psychologist';
+export const SITE_DESCRIPTION = 'Alissa is a developmental psychologist currently working with Dr. Michael Tomasello and Dr. Dorsa Amir at Duke University';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
