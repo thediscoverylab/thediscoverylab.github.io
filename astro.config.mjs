418f068a8cc8be3d0250256eee2e0ad5468f974b
Alissa Rivero <156251629+alissarvr@users.noreply.github.com>
2025-10-14 14:27:02 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alissarivero.github.io',
+  site: 'https://alissarvr.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
