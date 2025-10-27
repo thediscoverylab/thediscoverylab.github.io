53c608a7715a98b207a8dfbc7dee3a6094775042
Alissa Rivero <alissa.rivero@duke.edu>
2025-10-27 19:38:32 -0400

    renaming

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Alissa Rivero | Developmental Psychologist';
-export const SITE_DESCRIPTION = 'Alissa is a developmental psychologist currently working with Dr. Michael Tomasello and Dr. Dorsa Amir at Duke University';
+export const SITE_TITLE = 'Imaginarium | The Discovery Lab';
+export const SITE_DESCRIPTION = 'A creative space for innovation and exploration.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
